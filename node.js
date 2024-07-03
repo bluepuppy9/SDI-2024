7493b4c712a33c8152d92672f8b108db458fcf8c
Ben.Bernadskiy <90880517+bluepuppy9123@users.noreply.github.com>
2024-07-02 23:55:07 -0400

    added ollama to node.js
    this should remove the need for a python file if everything runs from node.js

---
+++ node.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import fs from 'fs';
+import ollama from 'ollama';
+
+async function generateText() {
+    //const response = await ollama.generate({
+    //    model: 'llama3', // Specify the model you want to use
+    //    prompt: 'Why is the sky blue?', // Your prompt
+    //});
+    console.log("Hello World!");
+    //console.log(response.text); // Print the generated text
+}
+
+generateText();
+//const app = express();
+//app.get('/', (request, response) => {
+//    fs.readFile('index.html', 'utf8', (err, html) => {
+//
+//        if (err) {
+//            response.status(500).send('sorry, out of order');
+//        }
+//
+//        response.send(html);
+//    })
+//});
+//
+//app.listen(process.env.PORT || 5000, () => console.log('listening on port 5000'));
